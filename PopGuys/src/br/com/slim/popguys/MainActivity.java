5366f32311245bd27d4dc93edf8a6e4731b1c342
smiderle <ladair.junior@ciss.com.br>
2014-04-21 15:44:46 -0300

    s

--- PopGuys/src/br/com/slim/popguys/MainActivity.java
+++ PopGuys/src/br/com/slim/popguys/MainActivity.java
@@ -1,24 +1,170 @@
 package br.com.slim.popguys;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Base64;
+import android.util.Log;
 
-public class MainActivity extends Activity {
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // //
-    }
+public class MainActivity extends FragmentActivity {
+	
+	
+	private static final int SPLASH = 0;
+	private static final int SELECTION = 1;
+	private static final int FRAGMENT_COUNT = SELECTION +1;
 
+	private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	private boolean isResumed = false;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    getHash();
+	    
+	    uiHelper = new UiLifecycleHelper(this, callback);
+	    uiHelper.onCreate(savedInstanceState);
+
+	    setContentView(R.layout.activity_main);
+
+	    FragmentManager fm = getSupportFragmentManager();
+	    fragments[SPLASH] = fm.findFragmentById(R.id.splashFragment);
+	    fragments[SELECTION] = fm.findFragmentById(R.id.selectionFragment);
+
+	    FragmentTransaction transaction = fm.beginTransaction();
+	    for(int i = 0; i < fragments.length; i++) {
+	        transaction.hide(fragments[i]);
+	    }
+	    transaction.commit();
+	}
+
+	private void showFragment(int fragmentIndex, boolean addToBackStack) {
+	    FragmentManager fm = getSupportFragmentManager();
+	    FragmentTransaction transaction = fm.beginTransaction();
+	    for (int i = 0; i < fragments.length; i++) {
+	        if (i == fragmentIndex) {
+	            transaction.show(fragments[i]);
+	        } else {
+	            transaction.hide(fragments[i]);
+	        }
+	    }
+	    if (addToBackStack) {
+	        transaction.addToBackStack(null);
+	    }
+	    transaction.commit();
+	}
+	
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    // Only make changes if the activity is visible
+	    if (isResumed) {
+	        FragmentManager manager = getSupportFragmentManager();
+	        // Get the number of entries in the back stack
+	        int backStackSize = manager.getBackStackEntryCount();
+	        // Clear the back stack
+	        for (int i = 0; i < backStackSize; i++) {
+	            manager.popBackStack();
+	        }
+	        if (state.isOpened()) {
+	            // If the session state is open:
+	            // Show the authenticated fragment
+	            showFragment(SELECTION, false);
+	        } else if (state.isClosed()) {
+	            // If the session state is closed:
+	            // Show the login fragment
+	            showFragment(SPLASH, false);
+	        }
+	    }
+	}
+	
+	private UiLifecycleHelper uiHelper;
+	private Session.StatusCallback callback = 
+	    new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+	
+
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    uiHelper.onResume();
+	    isResumed = true;
+	}
+
+	@Override
+	public void onPause() {
+	    super.onPause();
+	    uiHelper.onPause();
+	    isResumed = false;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    uiHelper.onSaveInstanceState(outState);
+	}
+	
+	@Override
+	protected void onResumeFragments() {
+	    super.onResumeFragments();
+	    Session session = Session.getActiveSession();
+
+	    if (session != null && session.isOpened()) {
+	        // if the session is already open,
+	        // try to show the selection fragment
+	        showFragment(SELECTION, false);
+	    } else {
+	        // otherwise present the splash screen
+	        // and ask the person to login.
+	        showFragment(SPLASH, false);
+	    }
+	}	
+	
+	
+	private void getHash(){
+		try{
+	        Log.e("Checking signs", "");
+	        PackageInfo info = getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_SIGNATURES);
+	        for (Signature signature : info.signatures) {
+	            MessageDigest md = MessageDigest.getInstance("SHA");
+	            md.update(signature.toByteArray());
+	            Log.e("HASH",Base64.encodeToString(md.digest(), Base64.DEFAULT));
+	        }
+	    } catch (NameNotFoundException e) {
+	        e.printStackTrace();
+	        
+	    } catch (NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	        
+	    }
+	}
 }
